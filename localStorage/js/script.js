b2d70877721ea8c2db21e398c7a0d72d7687e931
Abid Al Wassie <abidalwassie@outlook.com>
2022-02-08 21:21:26 +0600

    updated project

--- localStorage/js/script.js
+++ localStorage/js/script.js
@@ -1,3 +1,27 @@
 window.onload = function() {
-  console.log(localStorage);
+  const inputKey = document.getElementById("inputKey");
+  const inputValue = document.getElementById("inputValue");
+  const btnInsert = document.getElementById("btnInsert");
+  const output = document.getElementById("output");
+
+
+  btnInsert.onclick = function () {
+    const key = inputKey.value;
+    const value = inputValue.value;
+
+    
+    if (key && value) {
+      localStorage.setItem(key, value);
+      location.reload();
+    }
+
+  };
+
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+
+    output.innerHTML += `${key}: ${value}<br/>`;
+  }
+
 }
